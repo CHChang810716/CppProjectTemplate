38eb596731db122655d23f24bafaf328fd4e061a
Chia-Hua Chang <CHChang810716@gmail.com>
2021-07-20 02:44:52 +0800

    update hunter, add some doc, fix MSVC warning

--- cmake/ide.cmake
+++ cmake/ide.cmake
@@ -1,2 +1,25 @@
+# akt_vscode_gen_c_cpp_properties(customized path c_cpp_properties.json.in)
+#
+# customized c_cpp_properties template content
+# {
+#     "configurations": [
+#         {
+#             "name": "AKT-default",
+#             "includePath": [
+#                 @AKT_VSCODE_C_CPP_INCLUDES@
+#             ],
+#             "defines": [
+#             ],
+#             "compilerPath": "@CMAKE_CXX_COMPILER@",
+#             "cStandard": "c@CMAKE_C_STANDARD@",
+#             "cppStandard": "c++@CMAKE_CXX_STANDARD@",
+#             "intelliSenseMode": "@AKT_VSCODE_COMPILER_ID@-x64"
+#         }
+#     ],
+#     "version": 4
+# }
+# Note that AKT_VSCODE_C_CPP_INCLUDES will include the hunter include path but not package include path.
+# Which means if the package include path is unusual and not at <hunter install path>/include then
+# such an include path will not included in AKT_VSCODE_C_CPP_INCLUDES.
 akt_vscode_gen_c_cpp_properties()
 akt_vscode_gen_launch()
--- cmake/scheme/basic_target_setting.cmake
+++ cmake/scheme/basic_target_setting.cmake
@@ -3,7 +3,9 @@ target_include_directories(${AKT_TARGET} PUBLIC
     $<INSTALL_INTERFACE:include>
 )
 target_link_libraries(${AKT_TARGET} PRIVATE Threads::Threads)
-target_compile_options(${AKT_TARGET} PUBLIC -ftemplate-backtrace-limit=0)
+if(GCC)
+    target_compile_options(${AKT_TARGET} PUBLIC -ftemplate-backtrace-limit=0)
+endif()
 if(UNIX)
     target_link_libraries(${AKT_TARGET} PRIVATE ${CMAKE_DL_LIBS} rt)
 endif()
