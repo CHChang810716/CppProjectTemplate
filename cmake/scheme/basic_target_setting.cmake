f86dbd42885a0f00c79eb46bff4707aebaad0330
Chia-Hua Chang <CHChang810716@gmail.com>
2021-07-28 03:02:40 +0800

    clean test cmake

--- cmake/scheme/basic_target_setting.cmake
+++ cmake/scheme/basic_target_setting.cmake
@@ -2,7 +2,6 @@ target_include_directories(${AKT_TARGET} PUBLIC
     $<BUILD_INTERFACE:${CMAKE_SOURCE_DIR}/src>
     $<INSTALL_INTERFACE:include>
 )
-akt_show_var(AKT_IMPORTED_TARGETS)
 target_link_libraries(${AKT_TARGET} PRIVATE ${AKT_IMPORTED_TARGETS})
 if(GCC)
     target_compile_options(${AKT_TARGET} PUBLIC -ftemplate-backtrace-limit=0)
@@ -10,4 +9,3 @@ endif()
 if(UNIX)
     target_link_libraries(${AKT_TARGET} PRIVATE ${CMAKE_DL_LIBS} rt)
 endif()
-akt_show_var(AKT_TARGET)
--- cmake/scheme/test.cmake
+++ cmake/scheme/test.cmake
@@ -1,9 +1,7 @@
-if(BUILD_TEST)
-    include(${CMAKE_CURRENT_LIST_DIR}/exe.cmake)
-    target_link_libraries(${AKT_TARGET} PRIVATE GTest::gtest_main)
-    add_test(
-        NAME ${AKT_TARGET}
-        COMMAND ${CMAKE_INSTALL_PREFIX}/bin/${AKT_TARGET}
-        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/workdir 
-    )
-endif()
+include(${CMAKE_CURRENT_LIST_DIR}/exe.cmake)
+target_link_libraries(${AKT_TARGET} PRIVATE GTest::gtest_main)
+add_test(
+    NAME ${AKT_TARGET}
+    COMMAND ${CMAKE_INSTALL_PREFIX}/bin/${AKT_TARGET}
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/workdir 
+)
