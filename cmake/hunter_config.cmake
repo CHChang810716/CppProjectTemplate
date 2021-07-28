ba31eace747ebb247cda2d85f9e16aeff3ef7a68
Chia-Hua Chang <CHChang810716@gmail.com>
2021-07-29 03:09:55 +0800

    integration test

---
+++ cmake/hunter_config.cmake
@@ -0,0 +1,7 @@
+# hunter_config(
+#     OpenCV
+#     VERSION "3.4.0-p0"
+#     CMAKE_ARGS 
+#         BUILD_SHARED_LIBS=ON
+#         ENABLE_PRECOMPILED_HEADERS=${OpenCV_ENABLE_PRECOMPILED_HEADERS}
+# )
--- cmake/packages.cmake
+++ cmake/packages.cmake
@@ -6,3 +6,27 @@ if(BUILD_TEST)
     akt_imported_targets_exclude(GTest::gtest_main GTest::gmock_main)
 endif()
 find_package(Threads REQUIRED)
+
+# Boost
+hunter_add_package(Boost COMPONENTS 
+    iostreams
+    program_options
+    serialization
+    system
+    filesystem
+)
+find_package(Boost CONFIG REQUIRED COMPONENTS
+    iostreams
+    program_options
+    serialization
+    system
+    filesystem
+)
+
+# OpenCV
+hunter_add_package(OpenCV)
+find_package(OpenCV CONFIG REQUIRED)
+
+# json
+hunter_add_package(nlohmann_json)
+find_package(nlohmann_json CONFIG REQUIRED)
