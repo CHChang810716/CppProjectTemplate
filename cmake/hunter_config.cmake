504ceaf2fc0472a877ddea0903b2d9467efd0a84
Chia-Hua Chang <CHChang810716@gmail.com>
2021-07-29 03:09:55 +0800

    integration test

--- cmake/hunter_config.cmake
+++ cmake/hunter_config.cmake
@@ -1,2 +1,8 @@
-
 hunter_config(Arkitekto GIT_SUBMODULE "lib/Arkitekto")
+# hunter_config(
+#     OpenCV
+#     VERSION "3.4.0-p0"
+#     CMAKE_ARGS 
+#         BUILD_SHARED_LIBS=ON
+#         ENABLE_PRECOMPILED_HEADERS=${OpenCV_ENABLE_PRECOMPILED_HEADERS}
+# )
